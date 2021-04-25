c9af372752e7b0323e20c163503e0a641b2bd31e
kaan0821 <kt1930@nyu.edu>
2021-04-25 19:32:00 +0800

    Profile Page
    就等对接了

---
+++ src/js/profile.js
@@ -0,0 +1,55 @@
+import React, {Component} from "react";
+import {Layout, Select, Row, Col} from 'antd';
+import "../css/index.css";
+import MainHeader from "../components/common/header";
+import MainFooter from "../components/common/footer";
+import IndexSearchWrapper from "../components/common/searchbar";
+import "../css/profile.css";
+import commentTable from '../components/common/commentTable';
+import {Table} from 'antd';
+import {data,columns,comments} from '../components/common/commentTable';
+
+const {Header,Content,Footer} = Layout;
+const {Option} = Select;
+
+class profile extends Component {
+    constructor(props) {
+        super(props);
+        const user = localStorage.getItem('userInfo');
+    }
+
+    componentDidMount() {
+        const commentContent = {comments};
+    }
+    
+    render() { 
+        return ( 
+            <Layout className="layout" style={{minHeight: "100%"}}>
+                <MainHeader />
+                <Content>   {/*这个div是头像和欢迎-->*/}
+                    <div style={{ display:"flex",justifyContent:'center',margin:'40px 0px'}}>
+                        <div>
+                            <img src = "/images/userprofile.jpeg" style={{display:"block",borderRadius:'80px'}} />
+                            <p style={{marginBottom:'2px'}}>student profile</p>
+                            <p>email : kaan@nyu.edu</p>
+                        </div>
+                        <div style={{marginLeft:'70px',marginTop:'50px'}}>
+                            <h1 id='profile-bannerText'>Kaan's Personal Page</h1>
+                            <p>You can check and access you comment history here</p>
+                        </div>
+                    </div>
+                   {/*这个div是comment历史表*/}
+                    <div className = "CommentSection" style={{margin:'40px 50px'}}>
+                        <Table columns={columns} expandable={{
+                            expandedRowRender: record => <p style={{ margin: 0,fontSize:'1rem' }}>{record.description}</p>,
+                            rowExpandable: record => record.name !== 'Not Expandable',
+                            }} dataSource={data}/>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2021 Created by Ant UED</Footer>
+            </Layout>
+         );
+    }
+}
+ 
+export default profile;
